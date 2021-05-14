6744bc08139bc9d98c45660fe1eb12bd5fdb37d9
Gina Yuan <gina.yuan@gmail.com>
2021-05-13 18:45:57 -0700

    render svg edges

--- src/index.ts
+++ src/index.ts
@@ -47,11 +47,34 @@ function dragElement(elem: HTMLElement) {
   }
 }
 
-dragElement(document.getElementById("draggable1"))
-dragElement(document.getElementById("draggable2"))
-dragElement(document.getElementById("draggable3"))
-
 const graph = document.getElementById("graph");
+const canvas = document.getElementById("canvas");
+
+function renderEdge(source_id: string, target_id: string) {
+  const source: HTMLElement = document.getElementById(source_id);
+  const target: HTMLElement = document.getElementById(target_id);
+  let x1 = source.offsetLeft + source.offsetWidth / 2;
+  let y1 = source.offsetTop + source.offsetHeight / 2;
+  let x2 = target.offsetLeft + target.offsetWidth / 2;
+  let y2 = target.offsetTop + target.offsetHeight / 2;
+  // https://dev.to/gavinsykes/appending-a-child-to-an-svg-using-pure-javascript-1h9g
+  let line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+  line.setAttribute('x1', x1.toString());
+  line.setAttribute('y1', y1.toString());
+  line.setAttribute('x2', x2.toString());
+  line.setAttribute('y2', y2.toString());
+  line.setAttribute('stroke', 'black');
+  line.setAttribute('stroke-width', '2px');
+  canvas.append(line);
+  console.log(line);
+}
+
+dragElement(document.getElementById("camera"))
+dragElement(document.getElementById("person_detection"))
+dragElement(document.getElementById("differential_privacy"))
+renderEdge("camera", "person_detection");
+renderEdge("person_detection", "differential_privacy");
+
 document.getElementById("figure4").onclick = function() {
 	graph.innerHTML = "";
 	graph.appendChild(figure4().render());
