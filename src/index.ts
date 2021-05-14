08cdb3a6402bea7da3a21bb404f2b1f52deda8e6
Gina Yuan <gina.yuan@gmail.com>
2021-05-13 18:04:08 -0700

    draggable elements

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,38 @@ function component() {
   return element;
 }
 
+function dragElement(elem: HTMLElement) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  document.getElementById(elem.id + "-header").onmousedown = dragMouseDown;
+
+  function dragMouseDown(e: MouseEvent) {
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e: MouseEvent) {
+     e.preventDefault();
+     pos1 = pos3 - e.clientX;
+     pos2 = pos4 - e.clientY;
+     pos3 = e.clientX;
+     pos4 = e.clientY;
+     elem.style.top = (elem.offsetTop - pos2) + "px";
+     elem.style.left = (elem.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement(e: MouseEvent) {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+dragElement(document.getElementById("draggable1"))
+dragElement(document.getElementById("draggable2"))
+dragElement(document.getElementById("draggable3"))
+
 const graph = document.getElementById("graph");
 document.getElementById("figure4").onclick = function() {
 	graph.innerHTML = "";
