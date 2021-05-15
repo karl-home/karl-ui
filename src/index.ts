0b55ed468a9b3b3df81199f662f1e642f608fea1
Gina Yuan <gina.yuan@gmail.com>
2021-05-15 10:48:48 -0700

    navbar

--- src/index.ts
+++ src/index.ts
@@ -5,53 +5,82 @@ import { Graph } from './graph';
 import { EdgeHTML } from './edge_html';
 import { ModuleHTML } from './module_html';
 
-function component() {
-  const element = document.createElement('div');
+const g = new Graph();
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+function initializeNavbar() {
+  let navbar = document.getElementsByClassName('navbar')[0]
+  let navbarElems = Array.from(navbar.getElementsByTagName('A'))
+    .map(elem => (elem as HTMLElement));
+  let navbarNames = navbarElems.map(elem => elem.getAttribute('name'));
+  navbarElems.forEach(function(elem) {
+    elem.onclick = function() {
+      // set active class
+      navbarElems.forEach(function(other) {
+        if (other.classList.contains('active')) {
+          other.classList.remove('active')
+        }
+      })
+      elem.classList.add('active')
 
-  return element;
+      // set container display
+      navbarNames.forEach(function(field) {
+        let id = field + '-container'
+        let container = document.getElementById(id)
+        if (field != elem.getAttribute('name')) {
+          container.style.display = 'none'
+        } else {
+          container.style.display = ''
+        }
+      })
+    }
+  })
+  navbarElems[0].click()
 }
 
-let g = new Graph();
-document.getElementById("figure4").onclick = function() {
-  figure4(g)
-};
-document.getElementById("figure5").onclick = function() {
-  figure5(g)
-};
-
-EdgeHTML.renderInitialForm(g)
-ModuleHTML.renderInitialForm(g)
-document.getElementById("button-A").onclick = function() {
-  g.add_sensor(SENSORS["camera"])
-};
-document.getElementById("button-B").onclick = function() {
-  g.add_module(MODULES["person_detection"])
-};
-document.getElementById("button-C").onclick = function() {
-  g.add_module(MODULES["differential_privacy"])
-};
-document.getElementById("button-D").onclick = function() {
-  g.add_data_edge({
-    stateless: true,
-    out_id: "camera",
-    out_ret: "motion",
-    module_id: "person_detection",
-    module_param: "image",
-  })
-};
-document.getElementById("button-E").onclick = function() {
-  g.add_data_edge({
-    stateless: true,
-    out_id: "person_detection",
-    out_ret: "count",
-    module_id: "differential_privacy",
-    module_param: "count",
-  })
-};
-document.getElementById('reset-button').onclick = function() {
-  g.reset()
+function initializeSidebar() {
+  EdgeHTML.renderInitialForm(g)
+  ModuleHTML.renderInitialForm(g)
+  document.getElementById('reset-button').onclick = function() {
+    g.reset()
+  }
 }
+
+function initializeExampleButtons() {
+  document.getElementById("figure4").onclick = function() {
+    figure4(g)
+  };
+  document.getElementById("figure5").onclick = function() {
+    figure5(g)
+  };
+  document.getElementById("button-A").onclick = function() {
+    g.add_sensor(SENSORS["camera"])
+  };
+  document.getElementById("button-B").onclick = function() {
+    g.add_module(MODULES["person_detection"])
+  };
+  document.getElementById("button-C").onclick = function() {
+    g.add_module(MODULES["differential_privacy"])
+  };
+  document.getElementById("button-D").onclick = function() {
+    g.add_data_edge({
+      stateless: true,
+      out_id: "camera",
+      out_ret: "motion",
+      module_id: "person_detection",
+      module_param: "image",
+    })
+  };
+  document.getElementById("button-E").onclick = function() {
+    g.add_data_edge({
+      stateless: true,
+      out_id: "person_detection",
+      out_ret: "count",
+      module_id: "differential_privacy",
+      module_param: "count",
+    })
+  };
+}
+
+initializeNavbar()
+initializeSidebar()
+initializeExampleButtons()
