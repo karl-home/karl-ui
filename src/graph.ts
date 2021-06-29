d9447440b665fa233256883ce04e08f32bd11d9e
Henry Weng <26154285+henryweng03@users.noreply.github.com>
2021-06-29 00:12:25 -0700

    Render graph with ranked nodes
    Use dp and recursion to find depth of each node, use dp and level order traversal to find horizontal position of each node

--- src/graph.ts
+++ src/graph.ts
@@ -3,6 +3,8 @@ import { EdgeHTML } from './sidebar/edge_html';
 import { ModuleList } from './sidebar/module_repo';
 import { SensorList } from './sidebar/sensor_html';
 import { Network } from './network';
+import { vCoordUnconnectedNode } from './main/graph_html';
+import { find } from 'lodash';
 
 export const NETWORK_NODE_ID: string = "NET";
 
@@ -156,7 +158,7 @@ export class Graph {
     GraphHTML.reset()
   }
 
-  add_sensor(sensor: Sensor): boolean {
+  add_sensor(sensor: Sensor, outTop: number, inTop: number, outLeft: number, inLeft: number): boolean {
     if (this._exists(sensor.id)) {
       console.error('sensor id already exists')
       return false
@@ -171,7 +173,7 @@ export class Graph {
         outgoing_buttons: [],
         incoming_buttons: [],
       }
-      let html = GraphHTML.renderSensor(sensor.id, inner);
+      let html = GraphHTML.renderSensor(sensor.id, inner, outTop, inTop, outLeft, inLeft);
       inner.htmlOut = html[0]
       inner.htmlIn = html[1]
       this.sensors[sensor.id] = inner;
@@ -220,10 +222,10 @@ export class Graph {
 
   add_module(mod: Module): void {
     let id = this._genEntityID(mod.globalId)
-    this.addModuleWithId(mod, id)
+    this.addModuleWithId(mod, id, -1, -1)
   }
 
-  addModuleWithId(mod: Module, id: string): void {
+  addModuleWithId(mod: Module, id: string, top: number, left: number): void {
     let inner: ModuleInner = {
       id: id,
       value: mod,
@@ -236,7 +238,7 @@ export class Graph {
       outgoing_buttons: [],
       incoming_buttons: [],
     }
-    inner.html = GraphHTML.renderModule(id, inner);
+    inner.html = GraphHTML.renderModule(id, inner, top, left);
     GraphHTML.renderModuleProperties(inner)
     ModuleList.addModule(id)
     this.modules[id] = inner;
@@ -636,17 +638,186 @@ export class Graph {
     return format;
   }
 
+
+  adjacencyMatrix = new Array()
+  nodeIDToIndex = new Map()
+  indexToNodeID = new Map()
+  nodeDepth: number[]
+  levelOrder: number[]
+  counterMap = new Map() //helper map for levelOrder
+  numNodes = 0
+  STEP_SIZE = 170
+  INITIAL_HEIGHT = 100
+  MAX_WIDTH = 800
+
   setGraphFormat(f: GraphFormat) {
     // TODO: only apply the delta
     this.reset()
-    f.sensors.forEach(sensor => this.add_sensor(sensor))
-    f.moduleIds.forEach(id => {
-      let mod = Network.checkModuleRepo(id.global)
-      this.addModuleWithId(mod, id.local)
-    })
+    this.numNodes = f.sensors.length * 2 + f.moduleIds.length
+
+    //mapping each node to an index to use in adj mat, getNodeDepth
+    for(let i = 0; i < f.sensors.length; i++){
+      this.nodeIDToIndex.set(f.sensors[i].id + 'OUT', 2 * i)
+      this.nodeIDToIndex.set(f.sensors[i].id + 'IN', 2 * i + 1)
+      this.indexToNodeID.set(2 * i, f.sensors[i].id + 'OUT')
+      this.indexToNodeID.set(2 * i + 1, f.sensors[i].id + 'IN')
+    }
+
+    for(let i = 0; i < f.moduleIds.length; i++){
+      this.nodeIDToIndex.set(f.moduleIds[i].local, i + f.sensors.length * 2)
+      this.indexToNodeID.set(i + f.sensors.length * 2, f.moduleIds[i].local)
+
+    }
+
+    //build adjacency matrix
+    for(let i = 0; i < this.numNodes; i++){
+      this.adjacencyMatrix[i] = new Array()
+      for(let j = 0; j < this.numNodes; j++){
+        this.adjacencyMatrix[i][j] = 0
+      }
+    }
+    
+    for(let i = 0; i < f.edges.data.length; i++){
+      if(this.nodeIDToIndex.get(f.edges.data[i].out_id + "OUT") == undefined){
+        this.adjacencyMatrix[this.nodeIDToIndex.get(f.edges.data[i].out_id)][this.nodeIDToIndex.get(f.edges.data[i].module_id)] = 1
+      } else {
+        this.adjacencyMatrix[this.nodeIDToIndex.get(f.edges.data[i].out_id + "OUT")][this.nodeIDToIndex.get(f.edges.data[i].module_id)] = 1
+      }
+    }
+
+    for(let i = 0; i < f.edges.state.length; i++){
+      this.adjacencyMatrix[this.nodeIDToIndex.get(f.edges.state[i].module_id)][this.nodeIDToIndex.get(f.edges.state[i].sensor_id + "IN")] = 1
+    }
+
+    this.nodeDepth = new Array()
+    for(let i = 0; i < this.numNodes; i++){
+      this.nodeDepth[i] = 0
+    }
+    for(let i = 0; i < this.numNodes; i++){
+      this.getNodeDepth(i)
+    }
+
+    this.counterMap = new Map()
+    //initializing heights for counterMap
+    for(let i = 0; i < this.numNodes; i++){
+      //if height currently is not in map
+      if(this.counterMap.get(this.nodeDepth[i]) == undefined){
+        this.counterMap.set(this.nodeDepth[i], 1)
+      }
+    }
+
+    this.levelOrder = new Array()
+    for(let i = 0; i < this.numNodes; i++){
+      this.levelOrder[i] = 0
+    }
+    for(let i = 0; i < this.numNodes; i++){
+      if(this.nodeDepth[i] == 100){
+        this.getLevelOrderTraversal(i)
+      }
+    }
+
+    console.log(this.levelOrder)
+
+    let numAtDepth = new Map()
+    for(let i = 0; i < this.numNodes; i++){
+      if(numAtDepth.get(this.nodeDepth[i]) == undefined){
+        numAtDepth.set(this.nodeDepth[i], 1)
+      } else {
+        numAtDepth.set(this.nodeDepth[i], numAtDepth.get(this.nodeDepth[i]) + 1)
+      }
+    }
+
+    let nodeHorizontal = new Array()
+    for(let i = 0; i < this.numNodes; i++){
+      nodeHorizontal[i] = this.MAX_WIDTH / (numAtDepth.get(this.nodeDepth[i]) + 1) * this.levelOrder[i]
+    }
+
+    vCoordUnconnectedNode.coord = 0
+
+    for(let i = 0; i < f.sensors.length; i++){
+      this.add_sensor(f.sensors[i], this.nodeDepth[2 * i], this.nodeDepth[2*i+1], nodeHorizontal[2 * i], nodeHorizontal[2*i + 1])
+    }
+    for(let i = 0; i < f.moduleIds.length; i++){
+      let mod = Network.checkModuleRepo(f.moduleIds[i].global)
+      this.addModuleWithId(mod, f.moduleIds[i].local, this.nodeDepth[f.sensors.length * 2 + i], nodeHorizontal[f.sensors.length * 2 + i])
+    }
+
     f.edges.data.forEach(edge => this.add_data_edge(edge))
     f.edges.state.forEach(edge => this.add_state_edge(edge))
     f.edges.network.forEach(edge => this.add_network_edge(edge))
     f.edges.interval.forEach(interval => this.set_interval(interval))
   }
+
+  getNodeDepth(nodeIndex: number){
+    if(this.nodeDepth[nodeIndex] != 0){
+      return this.nodeDepth[nodeIndex]
+    } 
+    let children = this.findNodeChildren(nodeIndex)
+    let parents = this.findNodeParent(nodeIndex)
+    if(children.length == 0){
+      if(parents.length == 0){
+        this.nodeDepth[nodeIndex] = -1
+        return -1
+      }
+    }
+    if(parents.length == 0){
+      this.nodeDepth[nodeIndex] = this.INITIAL_HEIGHT
+      return this.INITIAL_HEIGHT
+    }
+    else {
+      let max = 0;
+      parents.forEach(parent => {
+        max = Math.max(max, this.getNodeDepth(parent))
+      });
+      this.nodeDepth[nodeIndex] = max + this.STEP_SIZE
+      return max + this.STEP_SIZE
+    }
+  }
+
+  getLevelOrderTraversal(rootIndex: number){
+    if(this.levelOrder[rootIndex] != 0){
+      return this.levelOrder[rootIndex]
+    }
+    let queue = new Array()
+    queue.push(rootIndex)
+    while(queue.length != 0){
+      let dq = queue.shift()
+      if(this.levelOrder[dq] == 0){
+        this.levelOrder[dq] = this.counterMap.get(this.nodeDepth[dq])
+        console.log(this.indexToNodeID.get(dq))
+        console.log(this.levelOrder)
+        this.counterMap.set(this.nodeDepth[dq], this.counterMap.get(this.nodeDepth[dq]) + 1)
+        let children = this.findNodeChildren(dq)
+        children.forEach(element => {
+          queue.push(element)
+        });        
+      }
+      
+    }
+    
+  }
+
+  //returns indicies of the node's children
+  findNodeChildren(nodeIndex: number){
+    let result = new Array
+    for(let i = 0; i < this.numNodes; i++){
+      if(this.adjacencyMatrix[nodeIndex][i] == 1){
+        result.push(i)
+      }
+    }
+    return result
+  }
+
+  //returns indicies of the node's parents
+  findNodeParent(nodeIndex: number){
+    let result = new Array
+    for(let i = 0; i < this.numNodes; i++){
+      if(this.adjacencyMatrix[i][nodeIndex] == 1){
+        result.push(i)
+      }
+    }
+    return result
+  }
+
+
 }
