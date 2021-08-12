aeedfb97b51722cbb993862872ae22341a53c4a1
Jaylene <mjaylene@stanford.edu>
2021-08-12 01:37:35 -0700

    Created a functioning modal! However, it is lacking the actual legend content.

--- src/index.ts
+++ src/index.ts
@@ -40,6 +40,19 @@ function initializeNavbar() {
   })
   navbarElems[0].click()
 }
+function initializeLegend() {
+  var modal = document.getElementById("simpleModal");
+  var modalBtn = document.getElementById("modalBtn");
+  var closeBtn = document.getElementsByClassName("closeBtn")[0];
+  modalBtn.addEventListener("click", openModal);
+  closeBtn.addEventListener("click", closeModal);
+  function openModal() {
+    modal.style.display = "block";
+  }
+  function closeModal() {
+    modal.style.display = "none";
+  }
+}
 
 function initializeSidebar() {
   EdgeHTML.renderInitialForm(g)
@@ -92,3 +105,4 @@ initializeNavbar()
 initializeSidebar()
 initializeExampleButtons()
 initializeGraph()
+initializeLegend()
